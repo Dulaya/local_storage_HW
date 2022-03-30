0840fd8be5b7d361ac830764a630831e19039482
Dulaya <dulaya.saennok@gmail.com>
2022-03-29 22:43:54 -0700

    Screens: Separate screens from App.js

---
+++ src/screens/login/LoginScreen.style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const style = StyleSheet.create({
+  loginScreenContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginText: {
+    fontSize: 50,
+    color: '#333C83',
+  },
+});
